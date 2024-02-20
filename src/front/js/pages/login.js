13dab4673b6f09562bac775e4aff750ab724e081
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-02-20 16:39:42 +0000

    home page finished, other views defined

---
+++ src/front/js/pages/login.js
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+
+export const Login = () => {
+	const { store, actions } = useContext(Context);
+
+	return (
+		<div className="text-center mt-5">
+			
+		</div>
+	);
+};
