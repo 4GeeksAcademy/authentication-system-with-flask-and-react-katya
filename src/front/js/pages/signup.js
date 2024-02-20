263b0a0975940cf9ffa422cf04f5d169050ee026
t0kyolover <144109471+t0kyolover@users.noreply.github.com>
2024-02-20 19:25:49 +0000

    not fetching due to CORS

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,33 +1,49 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 
 export const Signup = () => {
   const { store, actions } = useContext(Context);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="container-fluid justify-content-center mt-4 w-25">
       <h4 className="text-warning mb-3 text-center">Sign Up</h4>
       <form>
-        <div class="form-floating mb-3">
+        <div className="form-floating mb-3">
           <input
             type="email"
-            class="form-control"
+            className="form-control"
             id="floatingInput"
             placeholder="name@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <label for="floatingInput">Email address</label>
+          <label htmlFor="floatingInput">Email address</label>
         </div>
-        <div class="form-floating mb-3">
+        <div className="form-floating mb-3">
           <input
             type="password"
-            class="form-control"
+            className="form-control"
             id="floatingPassword"
             placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <label for="floatingPassword">Password</label>
+          <label htmlFor="floatingPassword">Password</label>
         </div>
         <div className="d-flex justify-content-center">
-          <button type="submit" class="btn btn-success px-3 py-2">
+          <button
+            onClick={(e) => {
+              e.preventDefault();
+              actions.signup(email, password);
+              navigate("/login");
+            }}
+            type="submit"
+            className="btn btn-success px-3 py-2"
+          >
             Register
           </button>
         </div>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,60 @@
 const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+  return {
+    store: {
+      user: [],
+    },
+    actions: {
+      login: () => async (email, password) => {
+        const resp = await fetch(
+          `https://special-journey-5gq9ppq7vxgw3vg9v-3001.app.github.dev/api/token`,
+          {
+			mode: 'no-cors',
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+          }
+        );
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+        if (!resp.ok) throw Error("There was a problem in the login request");
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+        if (resp.status === 401) {
+          throw "Invalid credentials";
+        } else if (resp.status === 400) {
+          throw "Invalid email or password format";
+        }
+        const data = await resp.json();
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+        localStorage.setItem("jwt-token", data.token);
+
+        return data;
+      },
+
+      signup: (email, password) => {
+        fetch(
+          `https://special-journey-5gq9ppq7vxgw3vg9v-3001.app.github.dev/api/signup`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email, password }),
+          }
+        )
+          .then((response) => {
+            if (!response.ok) {
+              throw Error(response.status);
+            }
+            return response.json();
+          })
+          .then((data) => {
+            setStore({ user: data });
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+
+		alert("User created successfully")
+      },
+    },
+  };
 };
 
 export default getState;
